b11c13acd80c1b50e2ff45f2a6d4368f1c876547
aboongm <aboongm@yahoo.com>
2022-05-03 13:42:33 +0530

    Add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
+import NavBar from './components/NavBar';
 
 function App() {
-  return <Calculator />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<NavBar />}>
+          <Route path="calculator" element={<Calculator />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
