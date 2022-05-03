a821ec5598254f0eedb4c9f4a29cca38fb1f6d18
aboongm <aboongm@yahoo.com>
2022-05-03 13:44:15 +0530

    Add Home component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Calculator from './components/Calculator';
 import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<NavBar />}>
+          <Route index element={<Home />} />
           <Route path="calculator" element={<Calculator />} />
         </Route>
       </Routes>
